1cce56dd55afb9928be84620e17a40d4f18e32a8
Yixiong <yixiongd@student.unimelb.edu.au>
2018-09-28 18:44:18 +1000

    Update HandleClient.java
    The HandleClient class will read the messages from clients and add the USER name to the message and then add it to the messages queue, pass the queue to the Game class

--- HandleClient.java
+++ HandleClient.java
@@ -1,17 +1,21 @@
 import java.io.*;
 import java.net.Socket;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class HandleClient implements Runnable {
 	private Socket socket;
 	private ConnectionToClient client;
 	private ConcurrentLinkedQueue<String> messages;
+	private JSONParser parser = new JSONParser();
 
 	HandleClient(ConnectionToClient client,ConcurrentLinkedQueue<String> messages ) {
 		this.client = client;
 		this.messages = messages;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
@@ -19,11 +23,17 @@ public class HandleClient implements Runnable {
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 			InputStreamReader isr = new InputStreamReader(input);
 			BufferedReader br = new BufferedReader(isr);
-			int id = 1;
-			String username;
 			while (true) {
-
-				
+				if(br != null) {
+					try {
+						JSONObject messageJSON = (JSONObject) parser.parse(br);
+						messageJSON.put("USER", client.getUserName());
+						String messageString = messageJSON.toJSONString();
+						messages.add(messageString);
+					}catch(ParseException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
