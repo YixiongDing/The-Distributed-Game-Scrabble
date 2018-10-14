83a881676dde5ac9cb245dd24236ef302c5af572
Alan <43667348+alanistaken@users.noreply.github.com>
2018-10-14 17:59:33 +1100

    Add files via upload

---
+++ MyClient.java
@@ -0,0 +1,102 @@
+// Project Name: Distributed System Project 2
+// Team name: Onmyoji
+// Team member: Yixiong Ding, Guangzhe Lan, Sihan Liu, Wuang Shen, Zhenhao Yu 
+
+//MyClient.java is used to save the variable about connection and connect to the Serverpackage ScrabbleClient;
+package ScrabbleClient;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MyClient {
+    private static Socket socket;
+    private static BufferedWriter bw;
+    private static BufferedReader br;
+    private String address;
+    private String port;
+    private Boolean ready = true;
+    private String userName = "";
+
+    public MyClient(String address, String port, String userName) {
+        try {
+            this.userName = userName;
+            this.address = address;
+            this.port = port;
+            this.socket = new Socket(address, Integer.parseInt(port));
+
+        } catch (NumberFormatException e1) {
+            new MessageUI2("please use right format of addressa and port number");
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            new MessageUI2("this address is unkown");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            new MessageUI2("Can not connect to the Server, try another address and port number");
+        }
+    }
+
+    public void buildBufferWrite() {
+        // Send the message to the server
+        OutputStream os;
+        try {
+            os = this.socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            this.bw = new BufferedWriter(osw);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            this.ready = false;
+        }
+    }
+
+    public void buildBufferRead() {
+        // Get the return message from the server
+        InputStream is;
+        try {
+            is = this.socket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            this.br = new BufferedReader(isr);
+        } catch (Exception e) {
+            this.ready = false;
+        }
+    }
+
+    public BufferedWriter getBufferWrite() {
+        return this.bw;
+    }
+
+    public BufferedReader getBufferReader() {
+        return this.br;
+    }
+
+    public Boolean getReady() {
+        return this.ready;
+    }
+    
+    public void setReady(Boolean ready) {
+    	this.ready = ready;
+    }
+    public Socket getSocket() {
+        return this.socket;
+    }
+    public String getUserName() {
+        return this.userName;
+    }
+    public void close() {
+        {
+            // Closing the socket
+            try {
+                this.socket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Scoreboard.java
@@ -0,0 +1,55 @@
+// Project Name: Distributed System Project 2
+// Team name: Onmyoji
+// Team member: Yixiong Ding, Guangzhe Lan, Sihan Liu, Wuang Shen, Zhenhao Yu 
+package ScrabbleClient;
+// This is the window where the scoreboard window is coded.
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.Font;
+import java.util.ArrayList;
+
+import javax.swing.JTextField;
+import java.awt.Color;
+
+public class Scoreboard extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public Scoreboard(ArrayList<String> toPrint) {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JTextArea txtrScoreboard = new JTextArea();
+		txtrScoreboard.setBackground(Color.ORANGE);
+		txtrScoreboard.setForeground(Color.DARK_GRAY);
+		txtrScoreboard.setFont(new Font("Monospaced", Font.BOLD, 40));
+		txtrScoreboard.setText("SCOREBOARD");
+		contentPane.add(txtrScoreboard, BorderLayout.NORTH);
+
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+
+		JTextArea txtrUsername = new JTextArea();
+		txtrUsername.setBackground(Color.LIGHT_GRAY);
+		txtrUsername.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		txtrUsername.setLineWrap(true);
+		for (String a:toPrint) {
+		    txtrUsername.append(a+"\n");
+		}
+		scrollPane.setViewportView(txtrUsername);
+	}
+
+}
